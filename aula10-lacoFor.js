7cc20bb9f2f702c25af772d024b77e71730611bd
Patrick <patrick.staroin@escola.pr.gov.br>
2025-04-23 07:56:56 -0300

    Aula 10

---
+++ aula10-lacoFor.js
@@ -0,0 +1,24 @@
+// LAÇO DE REPETIÇÃO FOR
+let contador = 1;
+while(contador < 10){
+    console.log("alguma coisa")
+    contador = contador + 1
+}
+for(let c = 1; c<10; c++){
+    console.log(c + " alguma coisa");
+}
+// VALE COMO NOTA DE RECUPERÇÃO
+// 1) Crie um laço FOR que mostra a tabuada (de 1 a 10)
+//  do número armazenado na variável abaixo:
+let numero = 3;
+
+// 2) Crie um programa que recebe a idade de 10 pessoas,
+// conforme o array abaixo, e mostre a porcentagem de pessoas
+// maiores de 18 anos.
+let idades = [10, 35, 15, 20, 27, 17, 18, 54, 65, 12];
+
+// 3) Crie um programa que imprime um quadrado com *, 
+// de acordo com o número armazenado na variável:
+let tamanho_quadrado = 3;
+
+// 4) Altere o código da questão 3 para gerar um triângulo.
